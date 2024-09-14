3429d9d2ad37f3e37a53448ddabacb6a45544b47
Uduak <uetimfon@gmail.com>
2024-09-14 20:32:22 +0100

    a contactinfo file

--- src/address_book.py
+++ src/address_book.py
@@ -7,5 +7,17 @@ class ContactInfo:
     def add_phonenumber(self, phonenumber):
         self.phonenumbers.append(phonenumber)
 
+    def remove_phonenumber(self, phonenumber):
+        self.phonenumbers.remove(phonenumber)
+
+    def change_firstname(self, firstname):
+        self.firstname = firstname
+
+    def change_lastname(self, lastname):
+        self.lastname = lastname
+
+    def edit_phonenumber(self, phonenumber):
+        self.phonenumbers = [phonenumber]
+
+
     
-        

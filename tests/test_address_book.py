f286f899b959a4b4853e0aaa9704fe31d79331e6
Uduak <uetimfon@gmail.com>
2024-09-14 20:33:23 +0100

    test for the contactinfo file

--- tests/test_address_book.py
+++ tests/test_address_book.py
@@ -21,3 +21,39 @@ def test_add_phonenumber():
     contact_info.add_phonenumber("08023678080")
     assert contact_info.phonenumbers == ["08023678079", "08023678080"]
     
+
+def test_remove_phonenumber():
+    """
+    Asserts that phone number can be removed from contact info
+    """
+    contact_info = ContactInfo("Alice", "Eti-mfon", "08023678079")
+    contact_info.remove_phonenumber("08023678079")
+    assert contact_info.phonenumbers == []
+
+
+def test_change_firstname():
+    """
+    Asserts that firstname can be changed
+    """
+    contact_info = ContactInfo("Alice", "Eti-mfon", "08023678079")
+    contact_info.change_firstname("Uduak")
+    assert contact_info.firstname == "Uduak"
+
+
+def test_change_lastname():
+    """
+    Asserts that lastname can be changed
+    """
+    contact_info = ContactInfo("Alice", "Eti-mfon", "08023678079")
+    contact_info.change_lastname("Udofia")
+    assert contact_info.lastname == "Udofia"
+
+
+def test_edit_phonenumber():
+    """
+    Asserts that phonenumber can be edited
+    """
+    contact_info = ContactInfo("Alice", "Eti-mfon", "08023678079")
+    contact_info.edit_phonenumber("08012345678")
+    assert contact_info.phonenumbers == ["08012345678"]
+    
